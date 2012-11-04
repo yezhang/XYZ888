5c91bd36fa5b3d12a59751c162ed9da680b8ef67
yezhang <yezhang@163.com>
2012-11-04 15:04:57 +0800

    Data Driven Test

--- hello/src/test/GuessNumberGameTest.java
+++ hello/src/test/GuessNumberGameTest.java
@@ -2,6 +2,12 @@ import junit.framework.Assert;
 import static junit.framework.Assert.assertEquals;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
 
 /**
  * Created with IntelliJ IDEA.
@@ -10,8 +16,31 @@ import org.junit.Test;
  * Time: 下午4:13
  * To change this template use File | Settings | File Templates.
  */
+
+@RunWith(Parameterized.class)
 public class GuessNumberGameTest {
 
+    private String input;
+    private String expected;
+    private String serverNumber = "1234";
+
+    public GuessNumberGameTest(String input, String expected){
+        this.expected = expected;
+        this.input = input;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data(){
+        Object[][] d = {
+                {"1235","3a0b"},
+                {"1256","2a0b"},
+                {"1756","1a0b"},
+                {"5617","0a1b"},
+                {"1245","2a1b"},
+                {"1243","2a2b"}
+        };
+        return Arrays.asList(d);
+    }
     //todo 4a0b
     @Test
     public void should_return_4a0b_if_input_match_exactly(){
@@ -23,62 +52,11 @@ public class GuessNumberGameTest {
         Assert.assertEquals("4a0b",result);
     }
 
-    //todo 3a0b
-    @Test
-    public void should_return_3a0b_if_input_has_only_three_correct_numbers_in_right_place(){
-        String serverNumber = "1234";
-        GuessNumberGame guessNumber = new GuessNumberGame(serverNumber);
-        String userInput = "1235";
-        String result = guessNumber.validate(userInput);
-        assertEquals("3a0b",result);
-    }
 
-    //todo 2a0b
     @Test
-    public void should_return_2a0b_if_input_has_only_three_correct_numbers_in_right_place(){
-        String serverNumber = "1234";
+    public void test_with_different_inputs(){
         GuessNumberGame guessNumber = new GuessNumberGame(serverNumber);
-        String userInput = "1256";
-        String result = guessNumber.validate(userInput);
-        assertEquals("2a0b",result);
-    }
-
-    //todo 1a0b
-    @Test
-    public void should_return_1a0b_if_input_has_only_three_correct_numbers_in_right_place(){
-        String serverNumber = "1234";
-        GuessNumberGame guessNumber = new GuessNumberGame(serverNumber);
-        String userInput = "1756";
-        String result = guessNumber.validate(userInput);
-        assertEquals("1a0b",result);
-    }
-    //todo 0a1b
-    @Test
-    public void should_return_0a1b_if_input_include_one_correct_number_with_wrong_place(){
-        String serverNumber = "1234";
-        GuessNumberGame guessNumber = new GuessNumberGame(serverNumber);
-        String userInput = "5617";
-        String result = guessNumber.validate(userInput);
-        assertEquals("0a1b",result);
-    }
-
-    //todo 2a1b
-    @Test
-    public void should_return_2a1b_if_input_include_three_numbers_with_two_in_right_place_and_one_in_wrong_place(){
-        String serverNumber = "1234";
-        GuessNumberGame guessNumber = new GuessNumberGame(serverNumber);
-        String userInput = "1245";
-        String result = guessNumber.validate(userInput);
-        assertEquals("2a1b",result);
-    }
-
-    //todo 2a2b
-    @Test
-    public void should_return_2a2b_if_input_has_four_correct_numbers_with_half_in_right_place(){
-        String serverNumber = "1234";
-        GuessNumberGame guessNumber = new GuessNumberGame(serverNumber);
-        String userInput = "1243";
-        String result = guessNumber.validate(userInput);
-        assertEquals("2a2b",result);
+        String result = guessNumber.validate(this.input);
+        assertEquals(this.expected,result);
     }
 }
